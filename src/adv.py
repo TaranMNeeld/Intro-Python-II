fc2ac8a88c04a40dad26609ef98f8be7ede7a3c5
Taran <thetaranneeld@gmail.com>
2020-02-12 14:09:43 -0700

    filled out player and room classes, created a loop with some logic regarding player movement

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,7 @@
+import os
+import textwrap
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -7,18 +10,18 @@ room = {
                      "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+                    passages run north and east."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+                    into the darkness. Ahead to the north, a light flickers in
+                    the distance, but there is no way across the chasm."""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+                    to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+                    chamber! Sadly, it has already been completely emptied by
+                    earlier adventurers. The only exit is to the south."""),
 }
 
 
@@ -49,3 +52,29 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+player = Player('treasure', playing=1)
+
+possible_moves = {
+    'n': 'n_to',
+    's': 's_to',
+    'e': 'e_to',
+    'w': 'w_to'
+}
+
+while player.playing == 1:
+
+    player_input = input('Enter a cardinal direction: [n], [s], [e], [w], or [q] to quit\n')
+    move_input = player_input.lower()
+
+    if move_input == 'q':
+        break
+
+    attrs = vars(room[player.room])
+
+    for attr in attrs:
+        print(f'{attr}')
+
+    if hasattr(room[player.room], possible_moves['s']):
+        print(room[player.room].name)
+
--- src/player.py
+++ src/player.py
@@ -1,2 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, room, playing):
+        self.room = room
+        self.playing = playing
